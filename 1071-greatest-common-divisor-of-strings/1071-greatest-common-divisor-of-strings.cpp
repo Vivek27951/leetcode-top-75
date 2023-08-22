ffd7fa6db671ce8229e3ba092c23b6b7c83ed3c8
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-08-23 00:04:04 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (60.49%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -12,7 +12,7 @@ public:
     string gcdOfStrings(string s1, string s2) {
         if(s1+s2!=s2+s1)
             return "";
-        int g = gcd(s1.size(),s2.size());
-        return s1.substr(0,g);
+        int l = gcd(s1.size(),s2.size());
+        return s1.substr(0,l);
     }
 };
