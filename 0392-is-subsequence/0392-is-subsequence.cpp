7f7b4b34e05d5fae64f69a0cc2f88990b846af86
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-09-01 00:48:21 +0530

    Time: 3 ms (48.03%), Space: 6.3 MB (58.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,12 +4,11 @@ public:
         int n = s.size();
         int m = t.size();
         int j = 0,i=0;
-        for(i=0;i<n;i++){
-            char ch = s[i];
-            while(j<m && t[j]!=ch) j++;
+        while(i<n && j<m){
+            if(s[i]==t[j])
+                i++;
             j++;
         }
-        if(i==n && j<=m) return true;
-        return false;
+        return i==n;
     }
 };
