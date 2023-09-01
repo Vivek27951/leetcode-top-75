99f1cc575629e5eacbef99acd92b093c029d791c
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-09-02 02:00:43 +0530

    Time: 74 ms (57.65%), Space: 59.1 MB (29.86%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,23 +2,13 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int n = height.size();
-        int prefix[n];
-        prefix[0]=height[0];
-        int suffix[n];
-        suffix[n-1]=height[n-1];
-        for(int i=1;i<n;i++){
-            prefix[i]=max(prefix[i-1],height[i]);
-            suffix[n-1-i]=max(suffix[n-i],height[n-1-i]);
-        }
-        int start = 0, end = n-1;
+        int start = 0;
+        int end = n-1;
         int ans = INT_MIN;
         while(start<end){
-            ans = max(ans,(end-start)*min(prefix[start],suffix[end]));
-            if(prefix[start]<=suffix[end])
-                start++;
-            else
-                end--;
-            
+            ans = max(ans,(end-start)*min(height[start],height[end]));
+            if(height[start]<=height[end]) start++;
+            else end--;
         }
         return ans;
     }
