c7db03bfe547e9d3430f29cf185dc0fb5b46d0a4
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-09-02 02:06:12 +0530

    Time: 74 ms (57.65%), Space: 58.9 MB (76.67%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -7,8 +7,14 @@ public:
         int ans = INT_MIN;
         while(start<end){
             ans = max(ans,(end-start)*min(height[start],height[end]));
-            if(height[start]<=height[end]) start++;
-            else end--;
+            if(height[start]<=height[end]){
+                int temp = start;
+                while(start<end && height[start]<=height[temp]) start++;
+            }
+            else{
+                int temp = end;
+                while(start<end && height[end]<=height[temp]) end--;
+            }
         }
         return ans;
     }
