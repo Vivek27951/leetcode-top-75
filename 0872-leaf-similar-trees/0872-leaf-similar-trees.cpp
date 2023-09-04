db180fa24c02d070056538332d9e706b4ea5977e
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-09-04 12:27:41 +0530

    Time: 7 ms (19.79%), Space: 12.9 MB (18.07%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -27,10 +27,6 @@ public:
         vector<int>v2;
         helper(root1,v1);
         helper(root2,v2);
-        if(v1.size()!=v2.size()) return false;
-        for(int i=0;i<v1.size();i++){
-            if(v1[i]!=v2[i])return false;
-        }
-        return true;
+        return v1==v2;
     }
 };
