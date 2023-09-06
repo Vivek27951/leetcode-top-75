561e6d2eb1b2ace469fa602a4c525e9bda6afccf
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-09-06 19:56:46 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (77.80%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,15 +4,14 @@ class Solution {
         
         if(dp[i]!=-1)
             return dp[i];
-         int c1 = helper(nums,i+1,dp);
-
-           int c2 = helper(nums,i+2,dp)+nums[i];
+        int c1 = helper(nums,i+1,dp);
+        int c2 = helper(nums,i+2,dp)+nums[i];
         dp[i] = max(c1,c2);
         return dp[i];
     }
 public:
     int rob(vector<int>& nums) {
-        vector<int>dp(102,-1);
+        vector<int>dp(101,-1);
         return helper(nums,0,dp);
     }
 };
