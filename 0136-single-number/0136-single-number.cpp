436542517f2ebe9438f0faed31ada352dd45e75b
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-09-06 23:04:19 +0530

    Time: 14 ms (75.52%), Space: 16.9 MB (72.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        for(int x : nums){
+        for(int x : nums)
             ans^=x;
-        }
         return ans;
     }
 };
