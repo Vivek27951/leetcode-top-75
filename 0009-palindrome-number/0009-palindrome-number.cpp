5bde0946dfd3f6cf98efb6c4e781dd98e8e49a1c
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-09-15 21:31:38 +0530

    Time: 15 ms (30.75%), Space: 6.2 MB (7.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x);
-        reverse(s.begin(),s.end());
-        return s == to_string(x);
+        if(x<0) return false;
+        int res = 0;
+        int t = x;
+        while(x){
+            if(res>INT_MAX/10 || (res==INT_MAX/10 && x%10>7))
+                return false;
+            res = res*10 + x%10;
+            x=x/10;
+        }
+        return res==t;
     }
 };
+
+// 2147483647
+// 1000000001
