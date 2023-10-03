7c8b19e07b68ada2d8a88602f163073f65730558
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-10-03 19:05:16 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (28.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -8,7 +8,6 @@ public:
         while(c<=e){
             if(nums[c]==0){
                 swap(nums[c++],nums[s++]);
-                // if(nums[c]==0) c++;
             }else if(nums[c]==2){
                 swap(nums[c],nums[e--]);
             }else c++;
