d129a69333171104393947bf2c5c52d546099a4c
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2023-11-01 00:26:18 +0530

    Time: 3 ms (64.92%), Space: 9.1 MB (53.95%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -6,11 +6,9 @@ public:
         int s = 0;
         int e = n-1;
         while(s<e){
-            int m = (e-s)/2 + s;
-            if(nums[m]<nums[m+1])
-                s = m+1;
-            else
-                e = m;
+            int m = s + (e-s)/2;
+            if(nums[m]<nums[m+1]) s=m+1;
+            else e = m;
         }
         return s;
     }
