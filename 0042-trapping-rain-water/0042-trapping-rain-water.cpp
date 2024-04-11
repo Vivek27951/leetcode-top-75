a1e7f6ab2831cc43017988ca3de01fc689b072b3
Vivek-Singh <64417544+Vivek27951@users.noreply.github.com>
2024-04-11 13:24:18 +0530

    Time: 13 ms (36.02%), Space: 23.2 MB (25.34%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    vector<int> nextGreater(vector<int> arr){
-        int n = arr.size();
-        vector<int> nge(n, -1);
-        int maxi = arr[0];
-        for(int i=0; i<n; i++){
-            nge[i]=maxi;
-            if(arr[i]>maxi) maxi = arr[i];
-        }
-        return nge;
-    }
-    int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> pre = nextGreater(height);
-        vector<int> clone = height;
-        reverse(clone.begin(), clone.end());
-        vector<int> suff = nextGreater(clone);
-        reverse(suff.begin(), suff.end());
-        int res = 0;
-        for(int i=0; i<n; i++){
-            if(min(pre[i], suff[i])-height[i]<0)    continue;
-            else res += min(pre[i], suff[i])-height[i];
-        }
-        return res;
+    int trap(vector<int>& hi) {
+        int size = hi.size();
+	vector<int> prefix(size);
+	vector<int> suffix(size);
+	
+	// prefix & suffix
+	prefix[0] = hi[0];
+	suffix[size-1] = hi[size-1];
+	for(int i=1;i<size;i++){
+		prefix[i] = max(prefix[i-1],hi[i]);
+		suffix[size-1-i] = max(suffix[size-i],hi[size-i-1]);
+	}
+	
+	int answer = 0;
+	for(int i=0;i<size;i++){
+		answer += min(prefix[i],suffix[i]) - hi[i];
+	}
+	return answer;
     }
 };
